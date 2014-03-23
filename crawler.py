6547879305c098593dc76069c35a78d1a353a626
Erica Portnoy <eportnoy@princeton.edu>
2014-03-23 23:58:03 +0000

    turn off tumblr crawling

--- crawler.py
+++ crawler.py
@@ -3,7 +3,7 @@ from datetime import date
 from dateutil.rrule import rrule, DAILY
 import mongoclient
 
-CRAWL_TUMBLR = True
+CRAWL_TUMBLR = False
 
 # grabs at most 20 posts a day with tag lol
 # from tumblr and stores them in the database
@@ -11,14 +11,13 @@ def crawl_tumblr():
     if not CRAWL_TUMBLR:
         return
     # date incrementing code from stackoverflow
-    # a = date(2007, 3, 1)
-    a = date(2014, 1, 1) # starting with 2014 as a test
+    a = date(2014, 1, 1)
     b = date(2014, 3, 22)
 
     for dt in rrule(DAILY, dtstart=a, until=b):
-        posts = tumblr.get_posts(dt.date.month,
-                                 dt.date.day,
-                                 dt.date.year)
+        posts = tumblr.get_posts(dt.month,
+                                 dt.day,
+                                 dt.year)
         mongoclient.insert_posts(posts)
 
 crawl_tumblr()
