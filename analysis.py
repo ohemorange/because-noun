d680a17ec179059812b8d822c69ed435e5c86802
Erica Portnoy <eportnoy@princeton.edu>
2014-03-24 00:59:21 +0000

    added analysis module

--- analysis.py
+++ analysis.py
@@ -1,3 +1,9 @@
 from because_lib import *
 
 import mongoclient as m
+
+def pbc_count():
+    bc_posts = m.posts.find({'text': {'$regex':'.*because.*', '$options':'is'}})
+    pbc_posts = [post for post in bc_posts if has_because(post["text"])]
+    print len(pbc_posts)
+
