c5352928757b548390f16ecca56921c05b098b90
Erica Portnoy <eportnoy@princeton.edu>
2014-03-24 01:26:16 +0000

    Accept newline as sentence end

--- because_lib.py
+++ because_lib.py
@@ -3,20 +3,36 @@ import re
 
 p = re.compile('NN.*|JJ.*|VBG')
 
+# sometimes clause, sometimes sentence
+def sentence_has_because(sentence):
+    tokens = nltk.word_tokenize(sentence)
+    if len(tokens) <= 0:
+        return False
+    if tokens[-1] == ".":
+        tokens = tokens[:-1]
+    if len(tokens) < 2 or tokens[-2].lower() != "because":
+        return False
+    tagged = nltk.pos_tag(tokens) # tagged with part of speech                                                                          
+    (a,b) = tagged[-1]
+    if p.match(b) != None:
+        return True
+
 # takes text
 # returns whether or not the text contains a
 # use of prepositional because.
 # this version requires that a sentence with pbc
 # ends in pbc then a single permissible word.
 def has_because(text):
+    # ignore whitespace, use proper punctuation
     for sentence in nltk.sent_tokenize(text):
-        tokens = nltk.word_tokenize(sentence)
-        if tokens[-1] == ".":
-            tokens = tokens[:-1]
-        if len(tokens) < 2 or tokens[-2].lower() != "because":
-            continue
-        tagged = nltk.pos_tag(tokens) # tagged with part of speech
-        (a,b) = tagged[-1]
-        if p.match(b) != None:
+        result = sentence_has_because(sentence)
+        if result == True:
             return True
+
+    # use whitespace to mark clause end
+    for sentence in text.splitlines():
+        result = sentence_has_because(sentence)
+        if result == True:
+            return True
+
     return False
