cba7fbf3d3f86ae2d1af47d20dd7614a3aa944d3
Erica Portnoy <eportnoy@princeton.edu>
2014-04-16 04:36:45 +0000

    parsing one sentence at a time, but only load once

--- parser.py
+++ parser.py
@@ -5,23 +5,31 @@ def feed_to_parser(text):
 
 def spawn_stanford_parser():
     global p
-    p = subprocess.Popen('bash stanford-parser-full-2014-01-04/lexparser.sh -', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    p = subprocess.Popen('bash stanford-parser-full-2014-01-04/lexparser.sh -sentence newline -', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 def exit_parser():
-    p.stdin.flush()
     p.stdin.close()
 
-def single_parse(text):
+# will only parse the first sentence it receives
+def sentence_parse(text):
 #    cmd = "bash stanford-parser-full-2014-01-04/lexparser.sh -"
 #    return subprocess.check_output(cmd, shell=True, stdin=subprocess.PIPE)
-    spawn_stanford_parser()
     feed_to_parser(text)
-    exit_parser()
-    return p.stdout.read()
+    p.stdin.flush()
+    line = p.stdout.readline()
+    out = ""
+    while line != "\n":
+        out += line
+        line = p.stdout.readline()
+    return out
 
 def main():
     sys.stdout.flush()
-    print single_parse("This is a sample sentence. This is another.")
+    spawn_stanford_parser()
+    print sentence_parse("This is a sample sentence. This is another.")
+    print sentence_parse("This is a third sentence.")
+    # Should print the parses of sample and third.
+    exit_parser()
     sys.stdout.flush()
 
 main()
